386ae4b57d4187a729faba7343143ce2fb8c8a15
Bhavam Bokare <bhavambokare2008@outlook.com>
2022-10-06 20:48:16 +0530

    Playing with Variables

---
+++ Variables.java
@@ -0,0 +1,14 @@
+package CoreJava;
+
+public class Variables {
+    public static void main(String[] args) {
+        int num = 100;
+        float num_2 = 500f;
+        boolean num_3 = true;
+        String num_4 = "Java is fun!";
+        System.out.println(num);
+        System.out.println(num_2);
+        System.out.println(num_3);
+        System.out.println(num_4);
+    }
+}
